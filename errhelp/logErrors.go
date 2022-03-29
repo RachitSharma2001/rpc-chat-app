6cffc6253f6c32ecd15659f8a0d12b00f262cc7b
RachitSharma2001 <rachitsharma613@gmail.com>
2022-03-29 11:10:10 -0700

    Reorganized project and created docker file

--- errhelp/logErrors.go
+++ errhelp/logErrors.go
@@ -9,3 +9,15 @@ func ThrowPortListenErr(err error) {
 func ThrowServeErr(err error) {
 	log.Fatalf("Unable to listen serve: %v", err)
 }
+
+func ThrowDialErr(err error) {
+	log.Fatalf("Error while dialing port 5000: %v", err)
+}
+
+func ThrowCreateStreamErr(err error) {
+	log.Fatalf("Error while creating stream: %v", err)
+}
+
+func ThrowReceiveMsgErr(err error) {
+	log.Fatalf("Error while recieving messages: %v", err)
+}
